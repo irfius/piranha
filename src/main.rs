e3190b98391ed1f342b00a272a181ad5a6f14157
irfius <4282437+irfius@users.noreply.github.com>
2022-04-17 18:48:31 -0400

    new init hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing, Router, Server};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", routing::get(|| async { "Hello, World!" }));
+    Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
